010e2c5c243423f4b14695aba563b8e6dea204ee
YcYap <yowchun93@gmail.com>
2015-10-15 16:43:19 +0800

    made remember test pass

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,13 +3,14 @@ class SessionsController < ApplicationController
   def new
 
   end
-  
+
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       log_in(user)
+      params[:session][:remember_me] == '1' ? remember(user) : forget(user)
       # create token and put into database
-      remember(user)
+      # remember(user)
       redirect_to user
     else
       flash.now[:danger] = 'Invalid email/password combination'
